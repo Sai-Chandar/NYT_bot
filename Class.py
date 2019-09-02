0297d027620601c5792b09ec3504cf46ec91576f
Sai-Chandar <n.saichander@gmail.com>
2019-09-02 23:51:43 +0530

    Added code to loop and fixed bugs.

--- Class.py
+++ Class.py
@@ -24,6 +24,7 @@ class NYT:
 
 class Twitter:
     api = 0
+    since_id = None
 
     def twitter_Authentication(self, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
         self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -31,14 +32,21 @@ class Twitter:
         Twitter.api = tweepy.API(self.auth)
 
     def mentions(self, count =1):
-        self.since_id = None
-        if self.since_id == None:
+        if Twitter.since_id == None:
             self.recent_mention = Twitter.api.mentions_timeline(count = count)
-            self.since_id = self.recent_mention[0].id
+            Twitter.since_id = self.recent_mention[0].id
+            return self.recent_mention[0]
+        self.recent_mention = Twitter.api.mentions_timeline(since_id = self.since_id, count = count)
+        #print(self.recent_mention)
+        if len(self.recent_mention) == 0:
+            return None
         else:
-            self.recent_mention = Twitter.api.mentions_timeline(since_id = self.since_id, count = count)
-            self.since_id = self.recent_mention[0].id
-        return self.recent_mention[0].text, self.recent_mention[0].id
+            Twitter.since_id = self.recent_mention[0].id
+            return self.recent_mention[0]
 
     def tweet(self, status, id):
         Twitter.api.update_status(status, in_reply_to_status_id = id, auto_populate_reply_metadata = True)
+        self.limits = Twitter.api.rate_limit_status()                               #returns no of tokens left for this hour
+        #remain_search_limits = limits['resources']['search']['/search/tweets']['remaining']
+        self.remain_search_limits = self.limits['resources']['statuses']['/statuses/mentions_timeline']['remaining']
+        print( "remaining tokens left for this hour:", self.remain_search_limits )
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from hidden import *
 from Class import NYT, Twitter
+import time
 
 if __name__ == '__main__':
     nyt = NYT(NYT_API)
     Twitter = Twitter()
     Twitter.twitter_Authentication(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    mention, id = Twitter.mentions()
-    mention = mention.replace("@Nsaichandar","").strip()
-    hits = nyt.frequency(mention)
-    Twitter.tweet(hits,id)
+    while True:
+        mention = Twitter.mentions()
+        if mention == None:
+            print("waiting...")
+            time.sleep(60)
+            continue
+        else:
+            status = mention.text.replace("@Nsaichandar","").strip()
+            hits = "Number of NYT articles '{}' appeared: {}".format(status, nyt.frequency(status))
+            Twitter.tweet(hits, mention.id)
+            print("tweeted.")
