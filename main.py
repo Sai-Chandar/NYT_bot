d1b84a597a2755d1efffe6638b734a0ae1ff8c95
Sai-Chandar <n.saichander@gmail.com>
2019-09-06 01:39:46 +0530

    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     CONSUMER_KEY = os.environ('CONSUMER_KEY')
     CONSUMER_SECRET = os.environ('CONSUMER_SECRET')
     ACCESS_TOKEN = os.environ('ACCESS_TOKEN')
-    ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+    ACCESS_TOKEN_SECRET = os.environ('ACCESS_TOKEN_SECRET')
     nyt = NYT(NYT_API)
     Twitter = Twitter()
     Twitter.twitter_Authentication(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
